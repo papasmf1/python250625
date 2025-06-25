8217217314e6a95a9304686eca2653701de256b0
papasmf1 <papasmf1@gmail.com>
2025-06-25 14:21:06 +0900

    크롤링 - 파일에 저장

--- web2.py
+++ web2.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 #웹서버에 요청
 import urllib.request
 
+#파일에 저장
+f = open("clien.txt", "wt", encoding="utf-8")
 #페이지처리(0부터 9까지)
 for i in range(0,10):
     url = "https://www.clien.net/service/board/sold?&od=T31&category=0&po=" + str(i)
@@ -14,7 +16,9 @@ for i in range(0,10):
     for tag in list:
         title = tag.text.strip()
         print(title)
+        f.write(title + "\n")
 
+f.close() 
 
 # <span class="subject_fixed" data-role="list-title-text" title="[예약중] 아이패드 미니 7 / 애플 케어 플러스 / 애플펜슬 프로">
 # 			[예약중] 아이패드 미니 7 / 애플 케어 플러스 / 애플펜슬 프로
