41029502d219780f16e8d33ea18f283641d5186c
papasmf1 <papasmf1@gmail.com>
2025-06-25 13:56:23 +0900

    페이징처리

--- web2.py
+++ web2.py
@@ -4,13 +4,16 @@ from bs4 import BeautifulSoup
 #웹서버에 요청
 import urllib.request
 
-url = "https://www.clien.net/service/board/sold" 
-data = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(data, "html.parser")
-list = soup.find_all("span", attrs={"data-role": "list-title-text"})
-for tag in list:
-    title = tag.text.strip()
-    print(title)
+#페이지처리(0부터 9까지)
+for i in range(0,10):
+    url = "https://www.clien.net/service/board/sold?&od=T31&category=0&po=" + str(i)
+    print(url)
+    data = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(data, "html.parser")
+    list = soup.find_all("span", attrs={"data-role": "list-title-text"})
+    for tag in list:
+        title = tag.text.strip()
+        print(title)
 
 
 # <span class="subject_fixed" data-role="list-title-text" title="[예약중] 아이패드 미니 7 / 애플 케어 플러스 / 애플펜슬 프로">
